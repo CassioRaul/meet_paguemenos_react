4b832eb5cb6642d1097e183f4b19dc44755a8066
Leonardo <Leonardo.sousa.dev@gmail.com>
2023-08-01 17:10:48 -0300

    style do button colaborador

--- src/Components/Colaborador/MenuColaborador.js
+++ src/Components/Colaborador/MenuColaborador.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "../../Styles/FotodePerfil.css";
-
+import "../../Styles/MenuGerente.css";
 import PerfilColaborador from '../../Assets/Perfils/usuario.jpg';
 import { useColaboradores } from "../../hooks/useColaboradores";
 import { UserContext } from "../../context/UserContext";
@@ -13,7 +13,7 @@ function MenuColaborador() {
   const { idColaborador } = useContext(UserContext);
 
   return (
-    <div className="gerente__responsivo MenuColacorador">
+    <div className="gerente__responsivo MeuGerente">
 
       <div className="sesaoPerfil">
         <img className="fotoDePerfil" src={PerfilColaborador} alt="Foto perfil colaborador" />
@@ -21,28 +21,25 @@ function MenuColaborador() {
         {colaboradores.filter(colaborador => colaborador.collaborator_id == idColaborador).map(colaborador => {
           return (
             <div key={colaborador.collaborator_id}>
-              <h4 className="gerente__nome">{colaborador.collaborator_name}</h4>
-              <h6 className="gerente__cargo">{colaborador.collaborator_function}</h6>
+              
             </div>
           )
         })}
       </div>
-
       <div className="accordion accordion-flush " id="accordionFlushExample">
-        <Link to="/AColaborador" className="MenuColaborador__Link " >
-          <button className="accordion-button collapsed MenuColaborador_Button btn_home">
-            <i className="bi bi-journal-plus"></i>
-            <p>Agendar</p>
+        <Link to="/AColaborador" className="MenuGerente__Link " >
+          <button className="accordion-button collapsed MenuGerente__Button btn__home">
+            <i className="bi bi-journal-plus" />Agendar
           </button>
         </Link>
-        <Link to="/PColaborador" className="MenuColaborador__Link" >
-          <button className="accordion-button collapsed MenuColaborador_Button btn_home">
+        <Link to="/PColaborador" className="MenuGerente__Link" >
+          <button className="accordion-button collapsed MenuGerente__Button btn__home">
             <i className="bi bi-clipboard2-data"></i>
             <p>Pdi</p>
           </button>
         </Link>
-        <Link to="/HColaborador" className="MenuColaborador__Link">
-          <button className="accordion-button collapsed MenuColaborador_Button btn_home">
+        <Link to="/HColaborador" className="MenuGerente__Link">
+          <button className="accordion-button collapsed MenuGerente__Button btn__home">
             <i className="bi bi-clock-history"></i>
             <p>Históricos</p>
           </button>
