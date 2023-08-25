9d23797b685a3bd2713fda3dd8b57fb0ce5ed2db
CassioRaul <cassioraulcastro@gmail.com>
2023-08-24 21:07:45 -0300

    API Hamachi

--- src/Service/ApiService.js
+++ src/Service/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const ApiService = axios.create({
-  baseURL: 'http://25.0.155.100:8000/',
+  baseURL: 'http://25.0.155.100:8000',
   headers: {
     'Content-Type': 'application/json'
   }
